86cb4a279e72a38aa7965069ab021b180d5977af
excalibur1702 <aryatathe@gmail.com>
2023-08-03 13:07:33 +0530

    background

--- src/index.js
+++ src/index.js
@@ -7,12 +7,15 @@ import app from "./App";
 
 import { ThemeProvider } from "@mui/material/styles";
 
+import CssBaseline from "@mui/material/CssBaseline";
+
 import theme from "./theme";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
+      <CssBaseline />
       <RouterProvider router={app} />
     </ThemeProvider>
   </React.StrictMode>
--- src/theme.js
+++ src/theme.js
@@ -2,6 +2,16 @@ import { createTheme } from "@mui/material/styles";
 
 const theme = createTheme({
   typography: {},
+  components: {
+    MuiCssBaseline: {
+      styleOverrides: {
+        body: {
+          background: "#2e282a",
+          color: "#ffc9b5",
+        },
+      },
+    },
+  },
 });
 
 export default theme;
